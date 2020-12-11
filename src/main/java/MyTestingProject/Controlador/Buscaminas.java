4d27f6b03a4f14a23eebb10ede12976c1781e3ab
carol11811 <carol11811@gmail.com>
2020-12-11 18:03:06 +0100

    Fix errors

---
+++ src/main/java/MyTestingProject/Controlador/Buscaminas.java
@@ -0,0 +1,137 @@
+package MyTestingProject.Controlador;
+
+import java.util.Scanner;
+
+import MyTestingProject.Modelo.Juego;
+import MyTestingProject.Modelo.Tablero;
+import MyTestingProject.Vista.vistaI;
+import MyTestingProject.Vista.vista;
+
+public class Buscaminas{
+	public static Tablero t;
+	public static vistaI vista;
+	public static Juego juego;
+	public Buscaminas(vistaI view) {
+		vista=view;
+	}
+	public Tablero getTablero() {return t;}
+	
+	public static void main(String[] args){
+		try (Scanner leer = new Scanner (System.in)) {
+			int horizontal;
+			int vertical;
+			int veces=0;
+			if(vista==null)
+				vista=new vista();
+			
+			do {
+				horizontal=vista.getHorizontal();
+				vertical=vista.getVertical();
+				t= new Tablero(horizontal,vertical,vista.getAleatorio(),vista.getMinas());
+				veces++;
+			}while(t.getCreated()==false && veces <3);
+			
+			if(t.getCreated()) {
+				
+
+				System.out.println("\n-x----------------------------------------------------x-");
+			   	System.out.println("  Ingresa las coordenas entre "+horizontal+ " y "+ vertical);
+			   	juego=new Juego();
+			  
+			   	juego.setMinas(vista.getMinas());
+			   	juego.setIntento(vista.getIntentos());
+			   	do {
+
+			   		
+			   		empezarJuego(horizontal,vertical,juego);
+			   	
+			   		
+			
+			  }while (juego.getIntento()>=juego.getMinas() && juego.getIntento()>0 && juego.getMinas()>0);	
+			   compruebaGanador(juego);
+			 
+
+			   		int x=0,y=0;
+			   	
+			   		
+			   		do{
+			   			 System.out.println("\n-x----------------------------------------------------x-");
+			   			 System.out.println("\n	Intento :"+juego.getIntento()+" , Cual es su coordenada? \n");
+			   			 x=vista.getPosX();
+			   			 System.out.println();
+			   			 y=vista.getPosY();
+			   			x=x-1;
+				   		y=y-1;
+			   			 
+			   			 if(x<0 || y<0 || x>=horizontal || y>=vertical) {
+			   				juego.setIntento(juego.getIntento()-1);
+			   			 	System.out.println("\n	==>Coordenadas incorrectas<== \n");
+			   			 }else {
+			   				 juego.jugar(t, x, y,horizontal,vertical);}
+			   		}while ((x<=0 || y<=0 || x>horizontal || y>vertical)&&juego.getIntento()>0&& juego.getMinas()>0);
+			   		
+			   		
+			   			
+			   		
+			
+			  }while (juego.getIntento()>=vista.getMinas() && juego.getIntento()>0 && juego.getMinas()>0);	
+			   
+			   	if(juego.getMinas()==0)
+			   		System.out.println("		GANASTE!");
+			   	
+			   	if(juego.getMinas()>juego.getIntento())
+			   		System.out.println("		PERDISTE!    (Exceso de minas restantes "+juego.getMinas()+")\n\n");
+			   	
+			   	if(juego.getIntento()==0)
+			   		System.out.println("		PERDISTE!");
+			   	
+			}
+		
+
+			}
+		
+       
+		
+		
+ 	   
+	
+	public static void compruebaGanador(Juego juego) {
+	  	if(juego.getMinas()==0)
+	   		System.out.println("		GANASTE!");
+	   	
+	   	if(juego.getMinas()>juego.getIntento())
+	   		System.out.println("		PERDISTE!    (Exceso de minas restantes "+juego.getMinas()+")\n\n");
+	   	
+	   	if(juego.getIntento()==0)
+	   		System.out.println("		PERDISTE!");
+		
+	}
+	
+	
+	public static void empezarJuego(int horizontal,int vertical,Juego juego) {
+		
+		int x=0,y=0;
+		
+   		do{
+   			 System.out.println("\n-x----------------------------------------------------x-");
+   			 System.out.println("\n	Intento :"+juego.getIntento()+" , Cual es su coordenada? \n");
+   			 x=vista.getPosX();
+   			 System.out.println();
+   			 y=vista.getPosY();
+   			x=x-1;
+   	   		y=y-1;
+   			 
+   			 if(x<=0 || y<=0 || x>horizontal || y>vertical)
+   			 	System.out.println("\n	==>Coordenadas incorrectas<== \n");
+   			 else
+   				 juego.jugar(t, x, y,horizontal,vertical);
+   		}while (x<=0 || y<=0 || x>horizontal || y>vertical);
+   		
+   	
+   		
+   		
+	}
+	
+	
+
+}
